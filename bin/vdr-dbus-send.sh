d1e30cece66362a6f9559ce8711cf46b27daf7a8
seahawk1986 <seahawk1986@gmx.de>
2021-11-30 13:09:28 +0100

    vdr-dbus-send.sh: add support for VDR instance id
    Use the VDR_ID environment variable if set (otherwise default to ID 0) and change the destination accordingly,
    so it is possible to choose a vdr instance by setting VDR_ID as an environment variable, e.g.
    ```shell
    VDR_ID=1 vdr-dbus-send /Plugins Plugin.List
    ```

--- bin/vdr-dbus-send.sh
+++ bin/vdr-dbus-send.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+declare -i VDR_ID=${VDR_ID:-0}
+
 if [ $# -lt 2 ]
 then
   echo "usage: $0 objectpath interface.method [arguments]"
@@ -7,6 +9,7 @@ then
 fi
 
 DEST=de.tvdr.vdr
+(( VDR_ID > 0 )) && DEST+="${VDR_ID}"
 OBJECT=$1
 shift
 INTERFACE=$1
